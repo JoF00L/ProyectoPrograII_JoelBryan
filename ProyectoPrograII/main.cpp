ca6e1d4ec36fd36c9c81d10f29987214a37dff56
BRYAN HERNANDEZ HERNANDEZ <bryan.hernandez.hernandez@est.una.ac.cr>
2023-04-25 00:00:02 -0600

    KLK Manito

--- ProyectoPrograII/main.cpp
+++ ProyectoPrograII/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
     cout << "Testing" << endl;
-  
+
     cout << "ESTA VIVOOO!!!" << endl;
 
     cout << "\nBryan favor conectarse >:(\n" << endl;
-    
-
+    cout << "Hola";
+    system("cls");
     cin.get();
     return 0;
 }
