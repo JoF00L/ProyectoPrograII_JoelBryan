478beb3ba7e53c4436e4bd35f901efdb298cbab7
JoF00L <131736806+JoF00L@users.noreply.github.com>
2023-05-09 23:10:14 -0600

    ggdbrfbgrfdgf

--- ProyectoPrograII/main.cpp
+++ ProyectoPrograII/main.cpp
@@ -2,6 +2,13 @@
 #include <iostream> 
 #include <windows.h>
 using namespace std;
+/*
+	05/09/2023
+	Programacion II - EIF204
+	Joel Ramirez Vargas - 119020788
+	Bryan Hernandez Hernandez - 402620925
+	Proyecto #1
+*/
 
 int main()
 {
